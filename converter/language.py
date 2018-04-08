b2f62b9ee6b26ead6f3353eaa6643429b56e5d19
Tejas-P-Herle <balaramtejas@gmail.com>
2018-04-08 18:08:17 +0530

    New Language Class

--- converter/language.py
+++ converter/language.py
@@ -1,6 +1,24 @@
 from mod_class import ModClass
 
 
-class (ModClass):
-    def __init__(self):
-        pass
+class Language(ModClass):
+
+    def __init__(self, name):
+        self.name = name
+
+    def check_file(self, file_name):
+        
+        try:
+            self.ext
+        except AttributeError:
+            err_code_cls, err_code_sub_cls, err_desc = '04', '01', '01'
+            err_code = err_code_cls + err_code_sub_cls + err_desc
+            Error(err_code, raise_error=True)
+        
+        if self.ext != self.get_ext(file_name):
+            return False
+
+        #TODO
+
+    def get_ext(file_name):
+        return file_name.split('.')[-1]
